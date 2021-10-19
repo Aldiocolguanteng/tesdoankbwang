96d90d93639ef2b19380afc9d4cc8b33187f68f1
KirBotz <tesheroku123@gmail.com>
2021-10-19 18:35:34 +0700

    TesDoank

---
+++ message/mess.js
@@ -0,0 +1,27 @@
+module.exports.mess = {
+	        wait: '*🌹𝑊𝑎𝑖𝑡...🌹*',
+			success: '_Sukses_',
+			regits : `── 「 *NOT REGISTERED* 」 ──
+
+_*Halo Kak Silahkan Verify Kirim Password Nya*_ *5557*
+
+*• Indonesian 🇲🇨*
+Silakan Register Terlebih Dahulu Sebelum Menggunakan Bot. 
+Cara Register Cukup Dengan Cara Memberikan Code & Password Bot
+
+*Note:*
+Harap Save Serial Number Mu Agar Bisa Melakukan Unreg Database Bot`,
+			wrongFormat: '```Format Salah, Coba Liat Lagi Di Menu```',
+			error: {
+				api: 'Maaf Terjadi Kesalahan',
+				stick: 'Bukan Sticker',
+				Iv: 'Link Tidak Valid'
+			},
+			only: {
+				group: 'Maaf! Command ini khusus untuk di dalam Group saja.',
+				admin: 'Maaf! Command ini khusus untuk admin group saja.',
+				premium: 'Kamu bukan user premium, kirim perintah *!buypremium* untuk membeli premium',
+				owner: 'Maaf! Command ini khusus untuk Owner Bot saja.',
+				Badmin: 'Maaf! Command ini khusus untuk Bot ketika jadi admin!!',
+			}
+		}
